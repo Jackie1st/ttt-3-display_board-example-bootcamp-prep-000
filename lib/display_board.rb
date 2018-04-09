6c280d59cb9a0517d294b57b2a618e04d1d3fbcd
Jackie First <jackiejeep@gmail.com>
2018-04-09 00:52:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-row = "   ", "|", "   ", "|", "   "
-separator = "-----------"
+
 def display_board
+  
   print row
   print separator
   print row
